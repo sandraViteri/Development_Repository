38662a5d0f8342db2a3c4a393f6252ce8f087bfe
Sandra Viteri <march1552@gmail.com>
2021-10-19 17:51:09 -0500

    Initialize project

---
+++ src/main/java/com/endava/store/storepets/StorepetsApplication.java
@@ -0,0 +1,13 @@
+package com.endava.store.storepets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorepetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorepetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endava/store/storepets/StorepetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.store.storepets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorepetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
